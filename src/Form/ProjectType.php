d84f1cdc7c472c0807a436c64093eff76082062f
smitbambharoliya <smitpatel32000@gmail.com>
2026-08-03 18:02:40 +0530

    feat: implement role-based dashboard redirection and add HR and employee portal interfaces

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -3,13 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Project;
-use App\Entity\Task;
-use Doctrine\DBAL\Types\DateType;
-use Doctrine\DBAL\Types\NumberType;
-use Doctrine\DBAL\Types\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
