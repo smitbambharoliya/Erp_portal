ffcd5c7a271e8f87530869b660c0b2e0ba6b41dd
smitbambharoliya <smitpatel32000@gmail.com>
2026-08-04 15:21:31 +0530

    fix: disable Dotenv loading in public/index.php when .env is missing

--- public/index.php
+++ public/index.php
@@ -2,8 +2,12 @@
 
 use App\Kernel;
 
+if (!is_file(dirname(__DIR__).'/.env')) {
+    $_SERVER['APP_RUNTIME_OPTIONS'] = ['disable_dotenv' => true];
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return static function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'] ?? 'prod', (bool) ($context['APP_DEBUG'] ?? false));
 };
